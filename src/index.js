ae8fd393ef90ed43c379c0ad38c6b5f03481c74d
QuentinLegeron <quentin.legeron@gmail.com>
2023-01-31 16:34:40 +0100

    je continue d'essayer de faire la root mais ça marche toujour pas

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Routes, Route} from "react-router-dom";
+import { BrowserRouter as Routes, Route} from "react-router-dom";
 import MovieDetails from "./MovieDetails";
 import reportWebVitals from './reportWebVitals';
 
+export default function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/movies/:id" element={<MovieDetails />} />
-  </Routes>
-);
+root.render(<Application />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
