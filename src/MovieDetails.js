a445638a988915e10c7c1ef4adbdfd7479a50bb3
Rayan - Laptop <75629737+IDRAYNAR@users.noreply.github.com>
2023-02-01 12:35:13 +0100

    Plus de details, amelioration des affichages, etc...

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,6 +1,7 @@
-import './MovieDetails.css'
+import "./MovieDetails.css";
 import React from "react";
 import { useParams } from "react-router-dom";
+import FlagIcon from "react-flag-icon-css";
 
 function MovieDetails() {
   const { id } = useParams();
@@ -20,6 +21,16 @@ function MovieDetails() {
     return <p>Loading...</p>;
   }
 
+  function formatCurrency(amount) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(amount);
+  }
+
+  const languageCode = movie.original_language;
+
   return (
     <div className="MovieDetails">
       <img
@@ -27,6 +38,41 @@ function MovieDetails() {
         alt={movie.title}
       />
       <h1>{movie.title}</h1>
+      <p>{movie.overview}</p>
+      <p>Rating: {movie.vote_average}</p>
+      <p>Release Date: {movie.release_date}</p>
+      <p>Runtime: {movie.runtime} minutes</p>
+      <p>Genres: {movie.genres.map((genre) => genre.name).join(", ")}</p>
+      <p>
+        Production Companies:{" "}
+        {movie.production_companies.map((company) => company.name).join(", ")}
+      </p>
+      <p>
+        Production Countries:{" "}
+        {movie.production_countries.map((country) => country.name).join(", ")}
+      </p>
+      <p>Tagline: {movie.tagline}</p>
+      <p>Revenue: {formatCurrency(movie.revenue)}</p>
+      <p>Budget: {formatCurrency(movie.budget)}</p>
+      <p>
+        Homepage: <a href={movie.homepage}>{movie.homepage}</a>
+      </p>
+      <p>IMDB ID: {movie.imdb_id}</p>
+      <p>
+        Original Language:
+        <FlagIcon code={languageCode.toLowerCase()} /> {languageCode}
+      </p>
+      <p>Original Title: {movie.original_title}</p>
+      <p>Popularity: {movie.popularity}</p>
+      <p>Status: {movie.status}</p>
+      <p>Video: {movie.video ? "Yes" : "No"}</p>
+      <p>Votes: {movie.vote_count}</p>
+      <p>Adult: {movie.adult ? "Yes" : "No"}</p>
+      <p>Backdrop Path: {movie.backdrop_path}</p>
+      <p>
+        Belongs To Collection:{" "}
+        {movie.belongs_to_collection ? movie.belongs_to_collection.name : "No"}
+      </p>
       <button onClick={() => window.history.back()}>Go Back</button>
     </div>
   );
